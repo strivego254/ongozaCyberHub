e0db3ceacea06ec0c8c3c688d000a65547a0ca8d
kiptoobarchok <kiptoobarchok>
2026-01-12 19:01:30 +0300

    docker

--- frontend/nextjs_app/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  // Enable standalone output for optimized Docker builds
-  output: 'standalone',
-}
-
-module.exports = nextConfig
-
